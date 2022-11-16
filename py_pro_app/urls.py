9aa88c41d44e9c7b411bf59858244686cf21adf8
Chandru <tochandru.engineer@gmail.com>
2022-11-16 13:49:09 +0530

    py compiler added

--- py_pro_app/urls.py
+++ py_pro_app/urls.py
@@ -4,15 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-
-
-
+    
     path('', views.home, name='home' ), 
     path('get_qa/<str:pk>/', views.qa_page, name='get_qa'), 
-
-
-
-
-
+    path('runcode/<str:pk>/', views.runcode, name='runcode'),
+    path('runcode_', views.runcode_, name = 'runcode_'), 
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- py_pro_app/views.py
+++ py_pro_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Topics, Question
 import json
+import sys
 
 # Create your views here.
 
@@ -15,8 +16,29 @@ def qa_page(request, pk):
     topics_objects = Topics.objects.all()
     data = Question.objects.filter(topic__title__contains=pk)
     # print(data)
-
-
     context = {'data': data, 'topics': topics_objects}
-
     return render(request, 'py_pro_app/qa.html', context)
+
+def runcode_(request):
+    if request.method =='POST':
+        code = request.POST['codearea']
+        try:
+            original = sys.stdout
+            sys.stdout = open('file.txt', 'w')
+            exec(code)
+            sys.stdout.close()
+            sys.stdout = original
+            output = open('file.txt', 'r').read()
+        except Exception as e:
+            sys.stdout = original
+            output = e
+        return render(request, 'pycompiler/py-compiler.html', {'code':code, 'output':output})
+
+    return render(request, 'pycompiler/py-compiler.html')
+
+
+
+def runcode(request, pk):
+
+    data = Question.objects.get(pk=pk)
+    return render(request, 'pycompiler/py-compiler.html', {'code': data.code})
